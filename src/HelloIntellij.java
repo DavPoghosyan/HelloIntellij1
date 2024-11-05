a5ef8923a815f905b90b6ab09d3f149fb48fefba
davit.poghosyan <davit.poghosyan@webbfontaine.com>
2024-11-05 12:15:25 +0600

    Add input parameter to HelloIntelliJ program

--- src/HelloIntellij.java
+++ src/HelloIntellij.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HelloIntellij {
     public static void main(String[] args) {
-        System.out.println("Hello, Intellij!");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a message to display:");
+        String message = scanner.nextLine();
+// add line to display message
+// add line to close scanner
+}}
